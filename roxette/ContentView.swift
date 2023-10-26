da518c7f9f3f70061ff8cd456285729d6c9ab8c7
Javier Godoy <rotvulpix@gmail.com>
2023-10-26 18:05:48 -0400

    feat: closes #1

--- roxette/ContentView.swift
+++ roxette/ContentView.swift
@@ -3,29 +3,59 @@ import SwiftUI
 struct ContentView: View {
     @EnvironmentObject var globalState: GlobalStateViewModel
     @State private var currentView: ViewsEnum = .home
-
+    
+    func isNearEdge(_ point: CGPoint, in size: CGSize) -> Void {
+        let edgeThreshold: CGFloat = 20.0
+        if(point.x <= edgeThreshold) {
+            withAnimation{
+                if(globalState.currentSidebar == .playlist) {
+                    globalState.currentSidebar = .none
+                } else {
+                    globalState.currentSidebar = .menu
+                }
+            }
+        } else if (point.x >= size.width - edgeThreshold) {
+            withAnimation{
+                print(globalState.currentSidebar == .menu)
+                if(globalState.currentSidebar == .menu) {
+                    globalState.currentSidebar = .none
+                } else {
+                    globalState.currentSidebar = .playlist
+                }
+            }
+        }
+    }
+    
     var body: some View {
-        ZStack(alignment: .leading) {
-            if globalState.currentSidebar == .menu {
-                MenuView(currentView: $currentView)
-                    .id("menu")
-            }
-
-            if globalState.currentSidebar == .playlist {
-                PlaylistView()
-                    .id("playlist")
-            }
-
-            MainView(currentView: $currentView)
-                .id("content")
-                .onTapGesture {
-                    withAnimation {
-                        if(globalState.isAnySidebarOpen()) {
-                            globalState.currentSidebar = .none
+        GeometryReader { geometry in
+            ZStack(alignment: .leading) {
+                if globalState.currentSidebar == .menu {
+                    MenuView(currentView: $currentView)
+                        .id("menu")
+                }
+                
+                if globalState.currentSidebar == .playlist {
+                    PlaylistView()
+                        .id("playlist")
+                }
+                
+                MainView(currentView: $currentView)
+                    .id("content")
+                    .onTapGesture {
+                        withAnimation {
+                            if(globalState.isAnySidebarOpen()) {
+                                globalState.currentSidebar = .none
+                            }
                         }
                     }
-                }
-        }.background(globalState.isAnySidebarOpen() ? Color.menuBackground : Color.white)
+            }.background(globalState.isAnySidebarOpen() ? Color.menuBackground : Color.white)
+                .gesture(
+                    DragGesture(minimumDistance: 0, coordinateSpace: .local)
+                        .onChanged { value in
+                            self.isNearEdge(value.startLocation, in: geometry.size)
+                        }
+                )
+        }
     }
 }
 
